d8349cd43073f2d982368b935d981ff8ddfdc099
OzcarDev <osja1601@gmail.com>
2023-10-13 20:06:36 -0600

    Ragdoll

---
+++ Assets/Project/Code/Scripts/Trajectory.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trajectory : MonoBehaviour
+{
+    [SerializeField] int dotsNumber;
+    
+    [SerializeField] GameObject dotsParent;
+    [SerializeField] GameObject dotPrefab;
+    [SerializeField] float dotSpacing;
+    [SerializeField] [Range(0.01f, 0.3f)] float dotMinScale;
+    [SerializeField] [Range(0.3f, 1f)] float dotmaxScale;
+    Transform[] dotsList;
+
+    Vector2 pos;
+
+    float timeStamp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hide();
+        PrepareDots();
+    }
+
+    void PrepareDots()
+    {
+        dotsList = new Transform[dotsNumber];
+        dotPrefab.transform.localScale = Vector3.one * dotmaxScale;
+        float scale = dotmaxScale;
+        float scaleFactor = scale / dotsNumber;
+        for (int i = 0; i < dotsNumber; i++)
+        {
+            dotsList[i] = Instantiate(dotPrefab, null).transform;
+            dotsList[i].parent = dotsParent.transform;
+
+            dotsList[i].localScale = Vector3.one * scale;
+            if (scale > dotMinScale) scale -= scaleFactor;
+
+        }
+    }
+
+
+    // Update is called once per frame
+    public void UpdateDots(Vector3 playerPos, Vector2 forceApplied)
+    {
+        timeStamp = dotSpacing;
+        for (int i = 0; i < dotsNumber; i++)
+        {
+            pos.x = playerPos.x + forceApplied.x * timeStamp;
+            pos.y = (playerPos.y + forceApplied.y * timeStamp) - (Physics2D.gravity.magnitude * timeStamp * timeStamp) / 2f;
+
+            dotsList[i].position = pos;
+            timeStamp += dotSpacing;
+        }
+    }
+    public void Show()
+    {
+        dotsParent.SetActive(true);
+    }
+    public void Hide()
+    {
+        dotsParent.SetActive(false);
+    }
+
+}
