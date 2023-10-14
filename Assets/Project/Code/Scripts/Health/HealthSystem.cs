0a71f1d8a5d39aac0b6b5ca53275f30077d3f76c
Dante <martin.velazquez.luis.alfonso@gmail.com>
2023-10-13 18:32:54 -0600

    HealthSystem

--- Assets/Project/Code/Scripts/Health/HealthSystem.cs
+++ Assets/Project/Code/Scripts/Health/HealthSystem.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using RacTools.Variables;
 
 public class HealthSystem : MonoBehaviour
 {
-    [SerializeField] private int MaxHealth;
+    [SerializeField] private VariableReference<int> MaxHealth;
 
     [SerializeField] private UnityEvent OnDamageDone;
-    
+    [SerializeField] private UnityEvent OnDead;
+
+    public int Health => _health;
     private int _health;
+
+    private void Start()
+    {
+        _health = MaxHealth.Value;
+    }
 
     public void AddHealth(int addedHealth)
     {
+        var newHealth = _health + addedHealth;
+        if(newHealth < _health)
+            OnDamageDone?.Invoke();
         
+        _health = Mathf.Clamp(newHealth, 0, MaxHealth.Value);
+        //_health = newHealth;
+        if(_health <= 0)
+            OnDead?.Invoke();
+    }
+    
+    [ContextMenu("Take Damage")]
+    public void TakeDamage()
+    {
+        if (!Application.IsPlaying(this)) return;
+        AddHealth(-1);
     }
 }
