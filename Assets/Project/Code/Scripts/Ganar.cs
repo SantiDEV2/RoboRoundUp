3085c6dd741024bfbaaa1ff0c86ec872b7d83871
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-10-31 23:58:00 -0600

    agregando win y lose

---
+++ Assets/Project/Code/Scripts/Ganar.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ganar : MonoBehaviour
+{
+
+    public GameObject[] robots;
+    public GameObject[] estrellas;
+    public GameObject panelWin,panelLose;
+    private int p;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        foreach (GameObject r in robots)
+        {
+            if (r.activeSelf)
+            {
+                Debug.Log(r);
+                
+                
+            }
+            else
+            {
+                Debug.Log(r + "ya no esta");
+                
+                if (Chariot.puntuacionpu == 3)
+                {
+                    panelWin.SetActive(true);
+                    estrellas[2].SetActive(true);
+                }
+                else if (Chariot.puntuacionpu == 2 && HealthSystem.puntuacionMala==1)
+                {
+                    panelWin.SetActive(true);
+                    estrellas[1].SetActive(true);
+                }
+                else if (Chariot.puntuacionpu == 1 && HealthSystem.puntuacionMala == 2)
+                {
+                    panelWin.SetActive(true);
+                    estrellas[0].SetActive(true);
+                }
+                else if (HealthSystem.puntuacionMala==3)
+                {
+                    panelLose.SetActive(true);
+                }
+            }
+
+        }
+
+
+       
+    }
+
+    
+}
